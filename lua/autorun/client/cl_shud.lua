8adce5cf12b5d25f10668703d55a7dbfe17da49e
Alexey <42507256+AlexeyQ13@users.noreply.github.com>
2020-06-15 04:57:57 +0300

    Update cl_shud.lua
    Добавлены Lerp`ы, оптимизирован код.

--- lua/autorun/client/cl_shud.lua
+++ lua/autorun/client/cl_shud.lua
@@ -3,7 +3,6 @@ Created by Alexey.
 Discord: Alexey#8463
 ]]
 
-
 surface.CreateFont("HUDfont",{
 	font = "Ebrima", 
 	extended = false,
@@ -22,45 +21,43 @@ surface.CreateFont("HUDfont",{
 	outline = true
 })
 
-
+HealthLerp = Localplayer:GetMaxHealth()
+ArmorLerp = 0
 
 function hud(  )
 
 	local ColorH = Color(215,120,120)
 	local ColorA = Color(120,120,215)
-	local healthH = LocalPlayer():Health()
-	local healthI = LocalPlayer():Health()
-	local armorA = LocalPlayer():Armor()
-	local armorI = LocalPlayer():Armor()
+	local health = LocalPlayer():Health()
+	local armor = LocalPlayer():Armor()
 
 	local width = 300
 	local height = 25
 
-	if healthH >= 100 then
-		healthH = 100
+	if health <= 0 then
+		health = 0
 	end
 
-	if healthI <= 0 then
-		healthI = 0
+	if armor >= 100 then
+		armor = 100
 	end
 
-	if armorA >= 100 then
-		armorA = 100
-	end
-
+	HealthLerp = Lerp(10*FrameTime(),HealthLerp,health)
+	ArmorLerp = Lerp(10*FrameTime(),ArmorLerp,armor)
+	
 	draw.RoundedBox(5,15,ScrH()-85,width+10,height+40,Color(180,180,180,140))
-
+	
 	--HealthBar
 	draw.RoundedBox(0,18,ScrH()-82,width+4,height+4,Color(28,28,28,220))
-	draw.RoundedBox(2,20,ScrH()-80,healthH*width/100,height,ColorH)
+	draw.RoundedBox(2,20,ScrH()-80,HealthLerp*width/100,height,ColorH)
 	draw.SimpleText("Health: " ,"HUDfont",height,ScrH()-68,Color(255,255,255,255),0,1) 	
-	draw.SimpleText(healthI ,"HUDfont",height+62,ScrH()-68,Color(255,255,255,255),0,1) 	
+	draw.SimpleText(health,"HUDfont",height+62,ScrH()-68,Color(255,255,255,255),0,1) 	
 
 	--ArmorBar
 	draw.RoundedBox(0,18,ScrH()-52,width+4,height+4,Color(28,28,28,220))
-	draw.RoundedBox(2,20,ScrH()-50,armorA*width/100,height,ColorA)
-	draw.SimpleText("Armor: " .. armorI,"HUDfont",height,ScrH()-38,Color(255,255,255,255),0,1) 
-	draw.SimpleText(armorI,"HUDfont",height+62,ScrH()-38,Color(255,255,255,255),0,1)
+	draw.RoundedBox(2,20,ScrH()-50,ArmorLerp*width/100,height,ColorA)
+	draw.SimpleText("Armor: " .. armor,"HUDfont",height,ScrH()-38,Color(255,255,255,255),0,1) 
+	draw.SimpleText(armor,"HUDfont",height+62,ScrH()-38,Color(255,255,255,255),0,1)
 end
 
 hook.Add("HUDPaint","SHUD",function()
@@ -73,5 +70,3 @@ hook.Add("HUDShouldDraw", "disableHUD", function(name)
 		if name == v then return false end
 	end
 end)
-
-print("SHUD loaded!")
